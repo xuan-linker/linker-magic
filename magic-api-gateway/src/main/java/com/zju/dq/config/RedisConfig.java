f29a018fcaf743b14ad4082e72c5e7e391cb8df2
xuanlccc@gmail.com <xuanlccc@gmail.com>
2021-03-05 11:22:20 +0800

    add

---
+++ magic-api-gateway/src/main/java/com/zju/dq/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.zju.dq.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Linker
+ * @date 2021/3/5 10:48
+ * @description：Redis相关配置
+ */
+@Configuration
+public class RedisConfig extends BaseRedisConfig {
+
+}
